431a392ff08b41b313834d299e66a55cc94abf3d
maciadalmau <47743853+maciadalmau@users.noreply.github.com>
2019-09-27 12:00:29 +0200

    Handout 1

---
+++ Physics2_class1_handout/ModulePhysics.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+
+class b2World;
+class b2BodyDef;
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+private:
+	
+	b2World* myWorld = nullptr;
+	b2BodyDef groundBodyDef;
+	bool debug;
+};
